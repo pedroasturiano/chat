9f73924ff39885c9597fe6af9857956cc011b9bd
pedro <pedroduartte2@gmail.com>
2020-09-21 14:32:26 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:chat/views/signin.dart';
+import 'package:chat/views/signup.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff145C9E),
+        scaffoldBackgroundColor: Color(0xff1F1F1F),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignUp(),
+    );
+  }
+}
