274ec2e6a7b6298bddc8a3fa1a3c75fca963fbfd
Pedro Duarte <pedroduartte2@gmail.com>
2020-09-25 08:21:34 -0300

    auth para login google

--- build/app/generated/not_namespaced_r_class_sources/debug/r/com/example/chat/R.java
+++ build/app/generated/not_namespaced_r_class_sources/debug/r/com/example/chat/R.java
@@ -266,7 +266,15 @@ public final class R {
     public static final int ic_launcher=0x7f080000;
   }
   public static final class string {
-    public static final int status_bar_notification_info_overflow=0x7f090000;
+    public static final int default_web_client_id=0x7f090000;
+    public static final int firebase_database_url=0x7f090001;
+    public static final int gcm_defaultSenderId=0x7f090002;
+    public static final int google_api_key=0x7f090003;
+    public static final int google_app_id=0x7f090004;
+    public static final int google_crash_reporting_api_key=0x7f090005;
+    public static final int google_storage_bucket=0x7f090006;
+    public static final int project_id=0x7f090007;
+    public static final int status_bar_notification_info_overflow=0x7f090008;
   }
   public static final class style {
     public static final int LaunchTheme=0x7f0a0000;
