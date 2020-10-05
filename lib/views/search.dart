cb8822d18a381475c0e750c65e4a77b8e8882cba
pedro <pedroduartte2@gmail.com>
2020-10-05 17:21:22 -0300

    commit 6

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -1,4 +1,6 @@
+import 'package:chat/services/database.dart';
 import 'package:chat/widgets/widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -7,8 +9,39 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  DatabaseMethods databaseMethods = new DatabaseMethods();
   TextEditingController searchTextEditingController =
       new TextEditingController();
+
+  QuerySnapshot searchSnapshot;
+
+  initiateSearch() {
+    databaseMethods
+        .getUserByUsername(searchTextEditingController.text)
+        .then((val) {
+      print(val.toString());
+      searchSnapshot = val;
+    });
+  }
+
+  Widget searchList() {
+    return searchSnapshot != null
+        ? ListView.builder(
+            itemCount: searchSnapshot.documents.length,
+            itemBuilder: (context, index) {
+              return SearchTile(
+                userName: searchSnapshot.documents[index].data["name"],
+                userEmail: searchSnapshot.documents[index].data["email"],
+              );
+            })
+        : Container();
+  }
+
+  @override
+  void initState() {
+    initiateSearch();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,22 +64,64 @@ class _SearchScreenState extends State<SearchScreen> {
                         hintStyle: TextStyle(color: Colors.white54),
                         border: InputBorder.none),
                   )),
-                  Container(
-                      height: 40,
-                      width: 40,
-                      decoration: BoxDecoration(
-                          gradient: LinearGradient(colors: [
-                            const Color(0x36FFFFFF),
-                            const Color(0x0FFFFFFF)
-                          ]),
-                          borderRadius: BorderRadius.circular(40)),
-                      padding: EdgeInsets.all(10),
-                      child: Image.asset("assets/images/search_white.png"))
+                  GestureDetector(
+                    onTap: () {
+                      initiateSearch();
+                    },
+                    child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                            gradient: LinearGradient(colors: [
+                              const Color(0x36FFFFFF),
+                              const Color(0x0FFFFFFF)
+                            ]),
+                            borderRadius: BorderRadius.circular(40)),
+                        padding: EdgeInsets.all(10),
+                        child: Image.asset("assets/images/search_white.png")),
+                  )
                 ],
               ),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SearchTile extends StatelessWidget {
+  final String userName;
+  final String userEmail;
+  SearchTile({this.userName, this.userEmail});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                userName,
+                style: simpleTextStyle(),
+              ),
+              Text(
+                userEmail,
+                style: simpleTextStyle(),
+              )
+            ],
+          ),
+          Spacer(),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.blue, borderRadius: BorderRadius.circular(30)),
+            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+            child: Text("Message"),
+          )
+        ],
       ),
     );
   }
